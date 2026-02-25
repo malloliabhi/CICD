262d54c28f1a95bebbe61d81253c014630758d43
Abhishek <malloliabhi@gmail.com>
2026-02-26 00:08:57 +0530

    adding test file

---
+++ test.py
@@ -0,0 +1,12 @@
+import pytest
+from fastapi.testclient import TestClient
+from app import app   # your FastAPI app
+
+@pytest.fixture
+def client():
+    return TestClient(app)
+
+def test_app_is_working(client):
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"message": "Hello World!"}
